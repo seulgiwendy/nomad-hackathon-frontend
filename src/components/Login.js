bea60a4347c15bc72d5714e81abaecf0e8dbb5fa
Jeewhan <distinct19@gmail.com>
2018-03-17 22:01:28 +0900

    Login 백그라운드 이미지 추가

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { withState } from "recompose";
 
+import "./Login.css";
+
 const enhance = withState("id", "setId", "");
 
 const Login = enhance(({ id, setId }) => (
-  <div>
+  <div className="Login">
     <input
       type="text"
       value={id}
