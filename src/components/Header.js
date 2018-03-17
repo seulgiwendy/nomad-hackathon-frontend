e5ae8e069690e7251b08eed11c152d93524ebfef
Jeewhan <distinct19@gmail.com>
2018-03-17 21:27:04 +0900

    Header

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Header = () => (
+  <header>
+    <NavLink to="/">Login</NavLink>
+  </header>
+);
+
+export default Header;
