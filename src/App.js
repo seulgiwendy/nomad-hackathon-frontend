7094e0dee912f1cf132ad44b88a32db4e8765a78
seulgiwendy <wheejuni@gmail.com>
2018-03-17 18:16:46 +0900

    [travis]변경사항 반영 테스트

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.<br/>
-		fuck! this will be my last travis ci experiment.
+		fuck! this will be my last travis ci experiment.<br/>
+		changes committed!
         </p>
       </div>
     );
