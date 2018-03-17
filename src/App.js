73d8062c222f981071f410f2f3b635a874662d71
seulgiwendy <wheejuni@gmail.com>
2018-03-17 17:43:03 +0900

    [travis] 실험 #2

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.<br/>
-		fuck!
+		fuck! this will be my last travis ci experiment.
         </p>
       </div>
     );
