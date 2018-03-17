3c8e5b2942d171a37c8632232cd62e1bc42f05c4
Jeewhan <distinct19@gmail.com>
2018-03-18 01:56:52 +0900

    Header에 CSS 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+import "./Header.css";
+
 const Header = () => (
-  <header>
+  <header className="header">
     <NavLink to="/upload">업로드</NavLink>
     <NavLink to="/">로그인 / 회원가입</NavLink>
   </header>
