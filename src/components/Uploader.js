1fdd934ad6a45027bfb7540e5236efc99953ce63
Jeewhan <distinct19@gmail.com>
2018-03-18 01:57:21 +0900

    Uploader를 antd Dragger로 변경

--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -1,21 +1,37 @@
 import React from "react";
-import DropzoneS3Uploader from "react-dropzone-s3-uploader";
+import { Upload, Icon, message } from "antd";
 
-export default class S3Uploader extends React.Component {
-  handleFinishedUpload = ({ filename, fileUrl }) => {
-    console.log(`File uploaded with filename ${filename}`);
-    console.log(`Access it on s3 at ${fileUrl}`);
-  };
+const props = {
+  name: "file",
+  multiple: true,
+  action: "//jsonplaceholder.typicode.com/posts/",
 
-  render() {
-    const s3Url = "https://my-bucket.s3.amazonaws.com";
+  onChange(info) {
+    const status = info.file.status;
 
-    return (
-      <DropzoneS3Uploader
-        onFinish={this.handleFinishedUpload}
-        s3Url={s3Url}
-        maxSize={1024 * 1024 * 5}
-      />
-    );
+    if (status !== "uploading") {
+      console.log(info.file, info.fileList);
+    }
+
+    if (status === "done") {
+      message.success(`${info.file.name} file uploaded successfully.`);
+    } else if (status === "error") {
+      message.error(`${info.file.name} file upload failed.`);
+    }
   }
-}
+};
+
+const Uploader = () => (
+  <Upload.Dragger {...props}>
+    <p className="ant-upload-drag-icon">
+      <Icon type="inbox" />
+    </p>
+    <p className="ant-upload-text">Click or drag file to this area to upload</p>
+    <p className="ant-upload-hint">
+      Support for a single or bulk upload. Strictly prohibit from uploading
+      company data or other band files
+    </p>
+  </Upload.Dragger>
+);
+
+export default Uploader;
